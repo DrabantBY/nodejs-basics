6f5a42f82d3c70f707d497150c9ab7cf89b09bc7
DrabantBY <drabant2022@gmail.com>
2024-10-30 00:28:56 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import { readdir } from "node:fs/promises";
+import getFilePath from "../utils/getFilePath.js";
+
 const list = async () => {
-    // Write your code here 
+	try {
+		const folder = getFilePath(import.meta.url, "files");
+		const files = await readdir(folder);
+		console.table(files);
+	} catch {
+		throw new Error("FS operation failed");
+	}
 };
 
 await list();
