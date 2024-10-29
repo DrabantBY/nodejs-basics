efa036d35db01dfc3c19d46bc20f17b1868e4071
DrabantBY <drabant2022@gmail.com>
2024-10-29 23:37:57 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from "node:fs/promises";
+import getFilePath from "../utils/getFilePath.js";
+
 const read = async () => {
-    // Write your code here 
+	try {
+		const targetFile = getFilePath(import.meta.url, "files/fileToRead.txt");
+		const text = await readFile(targetFile, { encoding: "utf8" });
+		console.log(text);
+	} catch {
+		throw new Error("FS operation failed");
+	}
 };
 
 await read();
