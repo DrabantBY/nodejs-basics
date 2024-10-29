76801320469102afa5b2e55b847702c64ec19a8f
DrabantBY <drabant2022@gmail.com>
2024-10-30 00:29:33 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import { rename as renameFile } from "node:fs/promises";
+import getFilePath from "../utils/getFilePath.js";
+
 const rename = async () => {
-    // Write your code here 
+	try {
+		const oldFile = getFilePath(import.meta.url, "files/wrongFilename.txt");
+		const newFile = getFilePath(import.meta.url, "files/properFilename.md");
+		await renameFile(oldFile, newFile);
+	} catch {
+		throw new Error("FS operation failed");
+	}
 };
 
 await rename();
