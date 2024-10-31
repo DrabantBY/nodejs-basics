e80c0e7d9377acd3c32090d2a8897289ebe67c43
DrabantBY <drabant2022@gmail.com>
2024-11-01 01:25:27 +0300

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+	const transformStream = new Transform({
+		transform(data, _, callback) {
+			const dataReversed = data.toString().split('').reverse().join('');
+
+			this.push(dataReversed + '\n');
+
+			callback();
+		},
+	});
+
+	process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
