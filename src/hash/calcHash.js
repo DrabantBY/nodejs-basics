1d7556133e98f83511692f6c9ac6270277d9281b
DrabantBY <drabant2022@gmail.com>
2024-10-31 23:16:57 +0300

    refactor: add pipeline to calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,6 +1,7 @@
 import { join } from 'node:path';
 import { createReadStream } from 'node:fs';
 import { createHash } from 'node:crypto';
+import { pipeline } from 'node:stream/promises';
 
 const calculateHash = async () => {
 	const file = join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
@@ -9,9 +10,11 @@ const calculateHash = async () => {
 
 	const hash = createHash('sha256');
 
-	input.pipe(hash).on('finish', () => {
-		console.log(hash.digest('hex'));
-	});
+	await pipeline(input, hash);
+
+	console.log(hash.digest('hex'));
 };
 
 await calculateHash();
+
+//! input.pipe(hash).setEncoding('hex').pipe(process.stdout) do not work!
