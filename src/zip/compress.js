1851783278c4e8496ce30ce463e2b551342183a6
DrabantBY <drabant2022@gmail.com>
2024-11-01 01:43:02 +0300

    feat: add compress/decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { join } from 'node:path';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+	const unzipFile = join(import.meta.dirname, 'files', 'fileToCompress.txt');
+	const zipFile = join(import.meta.dirname, 'files', 'archive.gz');
+
+	const readStream = createReadStream(unzipFile);
+	const gzipStream = createGzip();
+	const writeStream = createWriteStream(zipFile);
+
+	readStream.pipe(gzipStream).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { join } from 'node:path';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+	const unzipFile = join(import.meta.dirname, 'files', 'fileToCompress.txt');
+	const zipFile = join(import.meta.dirname, 'files', 'archive.gz');
+
+	const readStream = createReadStream(zipFile);
+	const unzipStream = createUnzip();
+	const writeStream = createWriteStream(unzipFile);
+
+	readStream.pipe(unzipStream).pipe(writeStream);
 };
 
 await decompress();
