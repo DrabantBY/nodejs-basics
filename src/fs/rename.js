603487366a02143bc3f9fa42cb967a033606150b
DrabantBY <drabant2022@gmail.com>
2024-11-01 13:48:29 +0300

    fix: add file existance checking

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,14 +1,26 @@
 import { join } from 'node:path';
-import { rename as renameFile } from 'node:fs/promises';
+import { rename as renameFile, access } from 'node:fs/promises';
 
 const rename = async () => {
 	try {
-		const oldFile = join(import.meta.dirname, 'files', 'wrongFilename.txt');
 		const newFile = join(import.meta.dirname, 'files', 'properFilename.md');
 
-		await renameFile(oldFile, newFile);
-	} catch {
+		await access(newFile);
+
 		throw new Error('FS operation failed');
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			try {
+				const newFile = join(import.meta.dirname, 'files', 'properFilename.md');
+				const oldFile = join(import.meta.dirname, 'files', 'wrongFilename.txt');
+
+				await renameFile(oldFile, newFile);
+			} catch {
+				throw new Error('FS operation failed');
+			}
+		} else {
+			throw err;
+		}
 	}
 };
 
